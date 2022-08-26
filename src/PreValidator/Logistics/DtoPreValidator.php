e6e6e952b448c6e558e7cc14366b53921ab660d2
Nikolay Nikolaev <evrinoma@gmail.com>
2022-08-26 20:09:19 +0200

    adjust prevalidator logistics

--- src/PreValidator/Logistics/DtoPreValidator.php
+++ src/PreValidator/Logistics/DtoPreValidator.php
@@ -22,33 +22,46 @@ class DtoPreValidator extends AbstractPreValidator implements DtoPreValidatorInt
 {
     public function onPost(DtoInterface $dto): void
     {
+        $this->checkDepart($dto);
+        $this->checkPackingList($dto);
+        $this->checkUser($dto);
     }
 
     public function onPut(DtoInterface $dto): void
     {
-        $this->checkIdDepart($dto);
-        $this->checkPackingListId($dto);
+        $this->checkDepart($dto);
+        $this->checkPackingList($dto);
+        $this->checkUser($dto);
     }
 
     public function onDelete(DtoInterface $dto): void
     {
-        $this->checkIdDepart($dto);
-        $this->checkPackingListId($dto);
+        $this->checkDepart($dto);
+        $this->checkPackingList($dto);
+        $this->checkUser($dto);
     }
 
-    private function checkIdDepart(DtoInterface $dto): void
+    private function checkUser(DtoInterface $dto): void
     {
         /** @var LogisticsApiDtoInterface $dto */
-        if (!$dto->hasIdDepart()) {
-            throw new ListItemInvalidException('The Dto has\'t IdDepart or class invalid');
+        if (!$dto->hasUserApiDto()) {
+            throw new ListItemInvalidException('The Dto has\'t User');
         }
     }
 
-    private function checkPackingListId(DtoInterface $dto): void
+    private function checkDepart(DtoInterface $dto): void
     {
         /** @var LogisticsApiDtoInterface $dto */
-        if (!$dto->hasPackingListId()) {
-            throw new ListItemInvalidException('The Dto has\'t PackingListId or class invalid');
+        if (!$dto->hasDepartApiDto()) {
+            throw new ListItemInvalidException('The Dto has\'t Depart');
+        }
+    }
+
+    private function checkPackingList(DtoInterface $dto): void
+    {
+        /** @var LogisticsApiDtoInterface $dto */
+        if (!$dto->hasPackingListApiDto()) {
+            throw new ListItemInvalidException('The Dto has\'t PackingList');
         }
     }
 }
